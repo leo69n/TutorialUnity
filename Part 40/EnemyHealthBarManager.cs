c5df8a9194ed5083c1299a32b7939fa22cf49b7e
Leo69 <qngnht@gmail.com>
2024-10-28 21:00:36 -0700

    Update EnemyHealthBarManager.cs

--- Part 40/EnemyHealthBarManager.cs
+++ Part 40/EnemyHealthBarManager.cs
@@ -14,6 +14,16 @@ public class EnemyHealthBarManager : MonoBehaviour
     void CreateHealthBar()
     {
         var HealthBarObject = Instantiate(HealthBarPrefab, HealthBarLocation.transform.position,Quaternion.identity,this.transform);
+        HealthBarManagerComponent = HealthBarObject.GetComponent<HealthBarManager>();
+        HealthBarManagerComponent.SetHealth(EnemyCurrentHealth, EnemyMaxHealth); //update Health Bar
     }
-    
+    public void UpdateHealth(float number)
+    {
+        EnemyCurrentHealth = EnemyCurrentHealth + number;
+        if (EnemyCurrentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        HealthBarManagerComponent.SetHealth(EnemyCurrentHealth, EnemyMaxHealth); //update Health Bar
+    }
 }
